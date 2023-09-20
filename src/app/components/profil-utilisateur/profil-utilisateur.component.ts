dc578aefdea65f5dc1a0c16a8b81c8c2724aa34f
Lydie Chevalier <didof69@hotmail.fr>
2023-09-20 11:47:10 +0200

    0.1 recuperation connexion/inscription utilisateur + profil utilisateur

--- src/app/components/profil-utilisateur/profil-utilisateur.component.ts
+++ src/app/components/profil-utilisateur/profil-utilisateur.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Utilisateur } from 'src/app/models/utilisateur';
+import { UtilisateurService } from 'src/app/services/utilisateur.service';
 
 @Component({
   selector: 'app-profil-utilisateur',
   templateUrl: './profil-utilisateur.component.html',
-  styleUrls: ['./profil-utilisateur.component.css']
+  styleUrls: ['./profil-utilisateur.component.css'],
 })
 export class ProfilUtilisateurComponent {
+  @Input() profil!: Utilisateur;
 
+  constructor(private utilisateurService: UtilisateurService) {}
+
+  getInitials(): string {
+    if (this.profil.nom && this.profil.prenom) {
+      return this.profil.nom.charAt(0) + this.profil.prenom.charAt(0);
+    }
+    return '';
+  }
 }
