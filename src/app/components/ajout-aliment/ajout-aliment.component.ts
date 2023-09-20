ed93420d4c8479ddd8333fc3e7602135f67872ec
Lydie Chevalier <didof69@hotmail.fr>
2023-09-20 12:27:27 +0200

    0.2 recuperation formulaire ajout aliment

--- src/app/components/ajout-aliment/ajout-aliment.component.ts
+++ src/app/components/ajout-aliment/ajout-aliment.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Aliment } from 'src/app/models/aliment';
+import { Restriction } from 'src/app/models/restriction';
+import { Saison } from 'src/app/models/saison';
+import { AlimentService } from 'src/app/services/aliment.service';
 
 @Component({
   selector: 'app-ajout-aliment',
   templateUrl: './ajout-aliment.component.html',
-  styleUrls: ['./ajout-aliment.component.css']
+  styleUrls: ['./ajout-aliment.component.css'],
 })
+  
 export class AjoutAlimentComponent {
+  newAliment!: Aliment;
 
-}
+  constructor(private alimentService: AlimentService) {}
+
+  createAliment(
+    // libelle: string,
+    // age_introduction: number,
+    // id_categorie: number,
+    // saisons: number[],
+    // restrictions: number[]
+    newAliment : Aliment
+  ) {
+    // let newAliment = {
+    //   libelle: libelle,
+    //   age_introduction: age_introduction,
+    //   id_categorie: id_categorie,
+    //   saisons: saisons,
+    //   restrictions: restrictions,
+    // };
+    // if (!libelle || !id_categorie || !age_introduction || !saisons) {
+    //   alert(`Merci de renseigner les champs vides`);
+    // } else {
+      this.alimentService.createAliment(newAliment).subscribe((data) => {
+        if (data.id) {
+          alert(`L'aliment id ${data.id} a été créée.`);
+        }
+      });
+    }
+  }
+
--- src/app/services/aliment.service.ts
+++ src/app/services/aliment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Aliment } from '../models/aliment';
 import { Observable } from 'rxjs';
 
@@ -7,9 +7,28 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AlimentService {
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) {}
+
+  setHeaders() {
+    const jwtToken = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${jwtToken}`,
+    });
+    return headers;
+  }
 
   getAliments(): Observable<Aliment[]> {
     return this.http.get<Aliment[]>('http://localhost:3000/api/aliments');
+  }
+
+  createAliment(aliment: Aliment): Observable<Aliment> {
+    // recup le token dans le localstorage
+    // const headers = this.setHeaders();
+    const headers = this.setHeaders();
+    return this.http.post<Aliment>(
+      `http://localhost:3000/api/aliments`,
+      aliment,
+      { headers }
+    );
   }
 }
