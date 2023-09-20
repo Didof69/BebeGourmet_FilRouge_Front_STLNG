1941f21c084bb392906d019edca7b29763d7c919
Lydie Chevalier <didof69@hotmail.fr>
2023-09-20 16:16:16 +0200

    0.4 card enfant / bouton ajout enfant redirection ok

--- src/app/components/card-enfant/card-enfant.component.ts
+++ src/app/components/card-enfant/card-enfant.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Enfant } from 'src/app/models/enfant';
 
 @Component({
   selector: 'app-card-enfant',
   templateUrl: './card-enfant.component.html',
-  styleUrls: ['./card-enfant.component.css']
+  styleUrls: ['./card-enfant.component.css'],
 })
 export class CardEnfantComponent {
-
+  @Input() enfant!: Enfant;
+  ngOnInit() {
+    console.log('les enfants', this.enfant.prenom);
+  }
 }
--- src/app/pages/utilisateur/utilisateur.component.ts
+++ src/app/pages/utilisateur/utilisateur.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Enfant } from 'src/app/models/enfant';
 import { Utilisateur } from 'src/app/models/utilisateur';
 import { UtilisateurService } from 'src/app/services/utilisateur.service';
 
@@ -9,26 +10,26 @@ import { UtilisateurService } from 'src/app/services/utilisateur.service';
   styleUrls: ['./utilisateur.component.css'],
 })
 export class UtilisateurComponent {
-  utilisateur!: Utilisateur ;
-  affichage: boolean = false //permet de savoir quel compsant affiché
+  utilisateur!: Utilisateur;
+  affichage: boolean = false; //permet de savoir quel compsant affiché
   // subUser$!: Observable<Utilisateur>; //à creuser
+  enfants?: Enfant[];
   
-  constructor(
-    private utilisateurService: UtilisateurService,
-  ) {}
+  constructor(private utilisateurService: UtilisateurService) {}
 
   ngOnInit(): void {
     // this.subUser$ = this.utilisateurService.getProfilUtilisateur(); //à creuser
     this.utilisateurService.getProfilUtilisateur().subscribe({
-        next: (response) => {
+      next: (response) => {
         console.log('retour get user:', response);
-        this.affichage = true //modifie le composant à afficher
-        this.utilisateur = response
+        this.affichage = true; //modifie le composant à afficher
+        this.utilisateur = response;
+        this.enfants = response.enfants;
       },
       error: (error) => {
-          this.affichage = true //modifie le composant à afficher
-          console.log("Echec get user", error);
-      }
-      });
+        this.affichage = true; //modifie le composant à afficher
+        console.log('Echec get user', error);
+      },
+    });
   }
 }
